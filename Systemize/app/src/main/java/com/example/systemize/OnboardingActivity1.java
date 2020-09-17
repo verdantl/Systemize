04913234fe92930c6f305fd61be90e9c3b92659d
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-09-16 21:13:25 -0400

    Pushed OnboardingActivity1

--- Systemize/app/src/main/java/com/example/systemize/OnboardingActivity1.java
+++ Systemize/app/src/main/java/com/example/systemize/OnboardingActivity1.java
@@ -2,16 +2,13 @@ package com.example.systemize;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class OnboardingActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, HomeScreenActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_onboarding1);
     }
 }
