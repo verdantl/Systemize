189b86f97254daa400c33521572c04caf3d0272c
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-09-17 22:45:21 -0400

    Changed fonts and added spinners to a new task activity

--- Systemize/app/src/main/java/com/example/systemize/MainActivity.java
+++ Systemize/app/src/main/java/com/example/systemize/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, HomeScreenActivity.class);
+        Intent intent = new Intent(this, NewTaskActivity.class);
         startActivity(intent);
     }
 }
