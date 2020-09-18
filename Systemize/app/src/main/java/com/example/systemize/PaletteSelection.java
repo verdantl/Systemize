a0fc976aa83ad5c7981ea7dd43fd8c62b4ac5d34
jimmy <yitaoli97@gmail.com>
2020-09-18 17:58:19 -0400

    colour palette finished

---
+++ Systemize/app/src/main/java/com/example/systemize/PaletteSelection.java
@@ -0,0 +1,14 @@
+package com.example.systemize;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaletteSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palette_selection);
+    }
+}
