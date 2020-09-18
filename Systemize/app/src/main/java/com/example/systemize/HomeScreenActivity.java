df52f7d6a511815fd7d98fdc06130edbaf6a2b19
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-09-17 21:26:05 -0400

    pushed the task list

--- Systemize/app/src/main/java/com/example/systemize/HomeScreenActivity.java
+++ Systemize/app/src/main/java/com/example/systemize/HomeScreenActivity.java
@@ -1,10 +1,17 @@
 package com.example.systemize;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -15,15 +22,33 @@ public class HomeScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        ArrayList<TaskItem> taskList = new ArrayList<>();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
-        taskList.add(new TaskItem("Title here", R.drawable.ic_baseline_home_24));
-
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        ListAdapter listAdapter = new ListAdapter(taskList);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(listAdapter);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.nav_calendar:
+                            selectedFragment = new CalendarFragment();
+                            break;
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_settings:
+                            selectedFragment = new SettingFragment();
+                            break;
+                        case R.id.nav_user:
+                            selectedFragment = new UserFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
