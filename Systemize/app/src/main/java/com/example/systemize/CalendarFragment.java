3816d0e9e4ded2244eeeb634adeaeae7b74922bc
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-09-18 20:18:35 -0400

    Renamed calendar and user fragments

--- Systemize/app/src/main/java/com/example/systemize/CalendarFragment.java
+++ Systemize/app/src/main/java/com/example/systemize/CalendarFragment.java
@@ -1,6 +1,9 @@
 package com.example.systemize;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,11 +11,86 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Objects;
 
 public class CalendarFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private ListAdapter listAdapter;
+    private ArrayList<TaskItem> taskList;
+    private String date;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        buildRecyclerView(view);
+
+        return view;
+    }
+
+    private void buildRecyclerView(View view){
+        recyclerView = view.findViewById(R.id.upcoming_tasks);
+//        readDatabase();
+        taskList = new ArrayList<>();
+        taskList.add(new TaskItem(1, "Title 1","", false ));
+        taskList.add(new TaskItem(1, "Title 2","", true ));
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        listAdapter = new ListAdapter(taskList, getResources().getFont(R.font.futura_medium));
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(listAdapter);
+
+        listAdapter.setOnItemClickListener(new ListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                taskList.get(position).changeCompleted();
+                System.out.println(taskList.get(position).getTitle());
+                System.out.println(taskList.get(position).getCompleted());
+            }
+        });
+    }
+
+    private void readDatabase(){
+        Calendar c = Calendar.getInstance();
+        LocalDate localDate = LocalDate.parse(date);
+
+        c.set(Calendar.YEAR, LocalDate.now().getYear());
+        c.set(Calendar.MONTH, LocalDate.now().getMonthValue());
+        c.set(Calendar.DAY_OF_MONTH, LocalDate.now().getDayOfMonth());
+        String date = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+
+        SQLiteDatabase db = new TaskHelper(Objects.requireNonNull(getActivity()).
+                getApplicationContext()).getReadableDatabase();
+        String sortOrder =
+                BaseColumns._ID + " DESC";
+
+        String selection = TaskContract.TaskEntry.COLUMN_NAME_DATE + " = ?";
+        String[] selectionArgs = { date};
+        Cursor cursor = db.query(
+                TaskContract.TaskEntry.TABLE_NAME,
+                null,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder);
+        taskList = new ArrayList<>();
+        while (cursor.moveToNext()){
+            int id = cursor.getInt(cursor.getColumnIndex(BaseColumns._ID));
+            String title = cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_NAME_TASK));
+            String category = cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_NAME_CATEGORY));
+            boolean completed = Boolean.parseBoolean(cursor.getString(
+                    cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_NAME_COMPLETED)));
+            taskList.add(new TaskItem(id, title, category, completed));
+        }
+        cursor.close();
+
     }
 }
